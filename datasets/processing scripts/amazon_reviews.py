b93197be8d535d035d05dc7e4aa43824f10f8e0e
Dean <Dean>
2019-03-06 00:17:37 +1300

    am

---
+++ datasets/processing scripts/amazon_reviews.py
@@ -0,0 +1,20 @@
+# "__label__1 corresponds to 1- and 2-star reviews, and __label__2 corresponds to 4- and 5-star reviews."  
+
+def load(path):
+    import bz2
+    import zipfile 
+
+    label_converter = {'__label__1': -1, '__label__2': 1}
+
+    archive = zipfile.ZipFile(path)
+    data = list()
+
+    for fname in archive.namelist():
+        bz = bz2.BZ2File(archive.open(fname), "r")
+        for line in bz.readlines():
+            line = line.decode('utf-8')
+            label, txt = line.split(' ', 1)
+            data.append({"input": txt, "label": label_converter[label]})
+        bz.close()
+    
+    return data
