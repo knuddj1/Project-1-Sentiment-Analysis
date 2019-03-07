80ff06a81ec8de852b533cf93b76b819e1ffd767
Dean Knudson (1000025498) <KNUDDJ1@student.op.ac.nz>
2019-03-07 19:16:42 +1300

    little bit of cleanup

--- datasets/loader.py
+++ datasets/loader.py
@@ -1,15 +1,22 @@
 import json
 import os
+import importlib.util
 
 path = "data_configs.json"
 
 configs = json.load(open(path))
 
 for dataset_name in configs.keys():
+    
     data_config = configs[dataset_name]
+
     if data_config["use"]:
-        import importlib.util
+
         spec = importlib.util.spec_from_file_location(dataset_name, data_config["loading_script"])
         module = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(module)
-        module.load(data_config["dataset_path"])
+        
+        if data_config["extra_params"] is not None:
+                data = module.load(data_config["dataset_path"], data_config["extra_params"])
+        else:
+                data = module.load(data_config["dataset_path"]) 
