f80e18f4fcf073a03d7ee2615e768477e4b490a6
Dean <Dean>
2019-03-12 01:22:58 +1300

    more loading work

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-from datasets import loader
+import datasets
+import time
 
-loader.get_data()
+
+total = 0
+start = time.time()
+
+for k, v in datasets.loader.load_data().items():
+    total += len(v)
+    print(k, len(v))
+    print()
+
+print(total)
+
+print(time.time() - start)
+
+
