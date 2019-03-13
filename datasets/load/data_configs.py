27cf44c5cf0cfe62342ef7b7ad89177d166d7472
Dean Knudson (1000025498) <KNUDDJ1@student.op.ac.nz>
2019-03-14 12:36:50 +1300

    stopped dataset_config being overwritten

--- datasets/load/data_configs.py
+++ datasets/load/data_configs.py
@@ -2,7 +2,7 @@ CONFIGS = {
 
     "Amazon_Reviews": {
         "use": True,
-        "dataset_path": "C:/Users/The Baboon/Desktop/BIT Y3 SM1/Project 1/datasets",
+        "dataset_path": None,
         "dataset_filename": "amazonreviews.zip",
         "loading_script": "amazon_reviews.py",
         "other_params": None
@@ -12,7 +12,7 @@ CONFIGS = {
 
     "Food_Reviews": {
         "use": True,
-        "dataset_path": "C:/Users/The Baboon/Desktop/BIT Y3 SM1/Project 1/datasets",
+        "dataset_path": None,
         "dataset_filename": "amazon-fine-food-reviews.zip",
         "loading_script": "food_reviews.py",
         "other_params": None
@@ -22,7 +22,7 @@ CONFIGS = {
 
     "Twitter_GOP_Debate": {
         "use": True,
-        "dataset_path": "C:/Users/The Baboon/Desktop/BIT Y3 SM1/Project 1/datasets",
+        "dataset_path": None,
         "dataset_filename": "first-gop-debate-twitter-sentiment.zip",
         "loading_script": "GOP_debate.py",
         "other_params": {
@@ -33,7 +33,7 @@ CONFIGS = {
 
     "IMDB": {
         "use": True,
-        "dataset_path": "C:/Users/The Baboon/Desktop/BIT Y3 SM1/Project 1/datasets",
+        "dataset_path": None,
         "dataset_filename": "aclImdb_v1.tar.gz",
         "loading_script": "imdb.py",
         "other_params": None
@@ -43,7 +43,7 @@ CONFIGS = {
 
     "Sentiment140": {
         "use": True,
-        "dataset_path": "C:/Users/The Baboon/Desktop/BIT Y3 SM1/Project 1/datasets",
+        "dataset_path": None,
         "dataset_filename": "trainingandtestdata.zip",
         "loading_script": "sentiment140.py",
         "other_params": None
@@ -53,7 +53,7 @@ CONFIGS = {
 
     "Twitter_Airline": {
         "use": True,
-        "dataset_path": "C:/Users/The Baboon/Desktop/BIT Y3 SM1/Project 1/datasets",
+        "dataset_path": None,
         "dataset_filename": "twitter-airline-sentiment.zip",
         "loading_script": "twitter_airline.py",
         "other_params": {
