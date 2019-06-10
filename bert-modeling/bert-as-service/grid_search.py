a8c82b0c11b48f6233e424f0489bc97451a443e9
Dean Knudson <knuddj1@student.op.ac.nz>
2019-06-10 21:39:38 +1200

    grid search complete

--- bert-modeling/bert-as-service/grid_search.py
+++ bert-modeling/bert-as-service/grid_search.py
@@ -1,55 +1,88 @@
 import json
 import numpy as np
+import itertools
+import os
 from load_bert_data import get_data
-
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
-from keras.losses import categorical_crossentropy
-
-
-def save(model, train_results):
-    results_dic = {
-        'acc':       train_results['acc'],
-        'loss':      train_results['loss'],
-        'val_acc':   train_results['val_acc'],
-        'val_loss':  train_results['val_loss'],
-    }
-    with open('results.json', 'w') as f:
-        json.dump(results_dic, f)
     
-    model_json = model.to_json()
-    with open("model.json", "w") as json_file:
-        json_file.write(model_json)
-    model.save_weights("model.h5")
-    print("Saved model to disk")
-    
-    
-
-# BERT Params ##########
-casing_types = ["uncased", "cased"]
-embed_sizes = [768, 1024]
-#######################
-
-# Trainer Params ######
-VALIDATION_SPLIT = 0.1
-BATCH_SIZE = 128
-NB_EPOCHS = 10
-SHUFFLE = True
-######################
-
-
-# Grid Search Params #####
-optimizers = ["sgd", "rmsprop", "adam", "adagrad", "adadelta", "adamax", "nadam"]
-dense_vals = [32, 64, 128, 256, 512]
-max_n_dense_layers = 10
+########## Grid Search Parameters #########################
+embed_sizes = [768, 1024] # BERT encoding  size
+validation_splits = [0.1, 0.2]
+batch_sizes = [64, 128, 256]
+optimizers = ["sgd", "rmsprop", "adam"]
 dropout_rates = [0.1, 0.2, 0.3, 0.4, 0.5]
-#########################
 
 
-X, y = get_data(casing_type=  , embed_size=  )
+### Generating all possible model layers ##
+dense_vals = [Dense(32), Dense(64), Dense(128), Dense(256)]
+max_n_dense_layers = 4
+layers = []
+
+for i in range(1, max_n_dense_layers + 1):
+    nl = [dense_vals]*i
+    variations = list(itertools.product(*nl))
+    layers += variations
+#####################################
+
+NB_EPOCHS = 5
+SHUFFLE = True
+
+### Creating gridsearch iterator 
+combinations = itertools.product(validation_splits, batch_sizes, optimizers, dropout_rates, layers)
+########################################################
 
 
-model = Sequential()
-model.compile(loss=categorical_crossentropy, optimizer='TODO', metrics=['accuracy'])
-model.fit(X, y, batch_size=BATCH_SIZE, shuffle=SHUFFLE, epochs=NB_EPOCHS,  validation_split=VALIDATION_SPLIT, verbose=1)
+### Verify path to save is correct
+save_dir = "R:\grid_search_results"
+if os.path.isdir(save_dir): os.mkdir(save_dir)
+model_save_dir = os.path.join(save_dir, "models")
+if os.path.isdir(model_save_dir): os.mkdir(model_save_dir)
+
+
+for e in embed_size:
+    X, y, test_sets = get_data(embed_size=e)
+
+    for vs, bs, opt, do, lc in combinations:
+        model = Sequential()
+        dense_layers_string = ""
+        for l in lc:
+            model.add(l)
+            model.add(Dropout(do))
+            dense_layers_string += "D-{0}".format(l.units)
+        model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=['accuracy'])
+        train_results = model.fit(X, y, batch_size=bs, shuffle=SHUFFLE, epochs=NB_EPOCHS,  validation_split=vs, verbose=1)
+
+        test_results = dict()
+        for dname, dset in test_sets.items():
+            test_results[dname] = model.evaluate(dset["X_test"], dset["y_test"])
+
+        model_save_path = "E-{0} VS-{1} BS-{2} OPT-{3} DO-{4} {5}".format(e, vs, bs, opt, do, dense_layers_string)
+        os.mkdir(model_save_path)
+
+        results_dic = {
+            'acc':       train_results['acc'],
+            'val_acc':   train_results['val_acc'],
+        }
+
+        for dname, acc in test_results.items():
+            results_dic[dname] = acc
+
+        with open(os.path.join(model_save_path,'results.json'), 'w') as f:
+            json.dump(results_dic, f)
+        
+        model_json = model.to_json()
+        with open(os.path.join(model_save_path,"model.json"), "w") as json_file:
+            json_file.write(model_json)
+        model.save_weights(os.path.join(model_save_path,"model.h5"))
+
+        model_params = {
+            "optimizer": opt,
+            "loss_function": "categorical_crossentropy"
+        }
+
+        with open(os.path.join(model_save_path,"model_params.json"), 'w') as f:
+            json.dump(results_dic, f)
+
+        
--- bert-modeling/bert-as-service/load_bert_data.py
+++ bert-modeling/bert-as-service/load_bert_data.py
@@ -1,13 +1,29 @@
 import json
 import numpy as np
 import pandas as pd
+import os
 from keras.utils import to_categorical
 
-def get_data(casing_type="uncased", embed_size=768):
-    bert_path = r"R:\custom_BERT_embedded\custom_training_set_BERT_encoded_{0}_{1}.json".format(casing_type, embed_size)
-    df = pd.read_json(bert_path)
+def load_and_process(path):
+    df = pd.read_json(path)
     df["input"] = df["input"].apply(lambda x: np.array(x))
     X = np.asarray(df["input"].tolist())
     y = df["label"].values
     y = to_categorical(y, num_classes=3)
-    return X, y 
+    return X, y
+
+
+def get_data(embed_size=768):
+    train_path = r"R:\custom_BERT_embedded\custom_training_set_BERT_encoded_uncased_{1}.json".format(embed_size)
+    X_train, y_train = load_and_process(train_path)
+
+    test_dir = r"R:\custom bert encoded\custom_test_set_BERT_encoded_uncased_{0}".format(embed_size)
+    test_fpaths = [os.path.join(test_dir, fpath) for fpath in os.listdir(test_dir)]
+    test_sets = dict()
+    
+    for fpath in test_fpaths:
+        dataset_name = fpath.split("\\")[-1].split("_")[0]
+        X_test, y_test = load_and_process(fpath)
+        test_sets[dataset_name] = {"X_test": X_test, "y_test": y_test}
+
+    return X_train, y_train, test_sets
